f26bdd1685af06be0a9eef9eca50840fa76ab6e8
wayne <keyawayne1@gmail.com>
2023-06-19 00:21:36 +0300

    assinment complete

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function updateEmployeeWithKeyAndValue(employee,key,value){
     };
     
 }
-function desructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
     employee[key] = value;
     return employee;
 }
